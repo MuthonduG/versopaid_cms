91a56a9f7fd628a9e2144b3456470f4d588c9b50
MuthonduG <muthondugithinji@gmail.com>
2026-03-18 20:00:16 +0300

    updates: updated db host

--- verso_cms/settings.py
+++ verso_cms/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'waitlist_db'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
